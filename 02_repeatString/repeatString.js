c0cfbe4aa6ef6bedd3d9e6220a5ba8e78f298f54
Triggsy1091 <ntriggs01@gmail.com>
2024-01-02 01:32:27 +1100

    Solution for exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var newString = '';
+    if (num < 0) {
+        return 'ERROR'
+    }
+    
+    for (let i = 0; i < num; i++) {
+        newString = newString + string;
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
