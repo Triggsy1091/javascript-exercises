0180d289e4f1bd7bc79c2eacee257a1076c18b9d
Triggsy1091 <ntriggs01@gmail.com>
2024-01-02 02:32:01 +1100

    solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9);
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahreheit = (celsius * (9 / 5) + 32);
+  fahreheit = Math.round(fahreheit * 10) / 10;
+  return fahreheit;
 };
 
 // Do not edit below this line
