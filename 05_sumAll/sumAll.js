baeb6359799de1f5a7942979eb7d70cbaf9edd7d
Triggsy1091 <ntriggs01@gmail.com>
2024-01-02 02:15:40 +1100

    Solution for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    var sum = 0;
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+
+    rangeArray = [];
+
+    for (let i = start; i <= end; i++) {
+        rangeArray.push(i);
+    }
+
+    rangeArray.forEach((value) => {
+        sum += value;
+    });
+
+    return sum;
 
 };
 
